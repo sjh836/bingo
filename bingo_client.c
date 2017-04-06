612cf2bf0c2c29682bc03dc7ec31325c81d0b819
sjh836 <sjh836@gmail.com>
2017-04-07 03:14:56 +0900

    어플 프로토콜 정의로 버그 해결, 과제 요구사항 해결

--- bingo_client.c
+++ bingo_client.c
@@ -10,18 +10,18 @@
 void error_check(int validation, char *message); //실행 오류 검사
 int value_check(int number); //점수 유효값 검사
 void game_init(); //빙고판 생성
-void game_run(int number, int turn_count); //게임진행
-void bingo_check();
+void game_print(int number, int turn_count); //게임진행
 
 int board[BOARD_SIZE][BOARD_SIZE]; //보드판 배열
-int turn[BOARD_SIZE*BOARD_SIZE]; //턴마다 숫자 기록
+//int turn[BOARD_SIZE*BOARD_SIZE]; //턴마다 숫자 기록
 int flag=1; //게임 승리여부
 int check_number[BOARD_SIZE*BOARD_SIZE+1]={0};
 int socket_fd;
+int turn[5];
 
 void main(int argc, char *argv[])
 {
-	int array_len, recv_len, recv_count, i;
+	int array_len, recv_len, recv_count, i, j;
 	struct sockaddr_in server_adr;
 
 	if(argc!=3)
@@ -45,32 +45,48 @@ void main(int argc, char *argv[])
 	
 	for(i=0;i<BOARD_SIZE*BOARD_SIZE&&flag;i++)
 	{
+		turn[0]=i+1;
+
 		if(i%2==0) //클라이언트 차례
 		{
-			printf("숫자를 입력해주세요 : ");
-			scanf("%d", &turn[i]);
-			turn[i]=value_check(turn[i]);
-			check_number[turn[i]]=1;
-
-			array_len=write(socket_fd, &turn[i], sizeof(turn[i])); //숫자를 서버로 전송
-			printf("%d 바이트를 전송하였습니다\n", array_len);
+			printf("%d턴 숫자를 입력해주세요 : ", i+1);
+			scanf("%d", &turn[3]);
+			turn[3]=value_check(turn[3]);
+			check_number[turn[3]]=1;
+			
+			array_len=write(socket_fd, turn, sizeof(turn));
+			printf("%d 바이트의 게임정보를 전송하였습니다\n", array_len);
 			error_check(array_len, "데이터전송");
 		}
+
 		else //서버 차례
 		{
 			recv_len=0;
-			while(recv_len!=sizeof(int)) // 패킷이 잘려서 올수도 있으므로 예외처리를 위한 조건문
+			while(recv_len!=sizeof(turn)) // 패킷이 잘려서 올수도 있으므로 예외처리를 위한 조건문
 			{
-				recv_count=read(socket_fd, &turn[i], sizeof(turn[i]));
+				recv_count=read(socket_fd, turn, sizeof(turn));
 				error_check(recv_count, "데이터수신");
-				if(recv_count==-0) break;
+				if(recv_count==0) break;
 				printf("%d 바이트를 수신하였습니다\n", recv_count);
 				recv_len+=recv_count;
 			}
-			check_number[turn[i]]=1;
+			check_number[turn[3]]=1;
+			
 		}
-		printf("turn[%d]=%d\n",i,turn[i]); //디버깅용
-		game_run(turn[i], i);
+		for(j=0;j<5;j++)
+			printf("turn[%d]=%d\n", j, turn[j]);
+		if(turn[1]==1)
+		{
+			printf("클라이언트 승리\n");
+			break;
+		}
+		else if(turn[1]==2)
+		{
+			printf("서버 승리\n");
+			break;
+		}
+		game_print(turn[3], i);
+		printf("=============\n");
 	}
 	close(socket_fd);
 
@@ -100,11 +116,20 @@ int value_check(int number) //점수 유효값 검사
 }
 void game_init()
 {
-	int check_number[BOARD_SIZE*BOARD_SIZE]={0};
 	int i, j; //카운트용 변수
+	int recv_len, recv_count;
+
+	recv_len=0;
+	while(recv_len!=sizeof(board)) // 패킷이 잘려서 올수도 있으므로 예외처리를 위한 조건문
+	{
+		recv_count=read(socket_fd, board, sizeof(board));
+		error_check(recv_count, "데이터수신");
+		if(recv_count==-0) break;
+		printf("%d 바이트의 board를 수신하였습니다\n", recv_count);
+		recv_len+=recv_count;
+	}
 
 	system("clear"); //화면 초기화
-	srand(time(NULL)+1); //시간을 인자로 주어서 완전한 랜덤을 구현
 
 	printf("%c[1;33m",27); //터미널 글자색을 노랑색으로 변경
 	
@@ -114,25 +139,14 @@ void game_init()
 	{
 		for(j=0; j < BOARD_SIZE; j++)
 		{
-			while(1)
-			{
-				int temp = rand()%25; //0~24 난수 발생
-				
-				if(check_number[temp]==0) //중복제거 알고리즘
-				{
-					check_number[temp]=1;
-					board[i][j]=temp+1;
-					printf("| %2d ", board[i][j]); 
-					break;
-				}
-			}
+			printf("| %2d ", board[i][j]); 
 		}
 		printf("|\n");
 		printf("+----+----+----+----+----+\n"); 
 	}      
 	printf("%c[0m", 27); //터미널 글자색을 원래색으로 변경
 }
-void game_run(int number, int turn_count)
+void game_print(int number, int turn_count)
 {
 	int i, j; //카운트용 변수
 
@@ -162,43 +176,4 @@ void game_run(int number, int turn_count)
 		printf("+----+----+----+----+----+\n"); 
 	}      
 	printf("%c[0m", 27); //터미널 글자색을 원래색으로 변경
-	bingo_check();
-}
-void bingo_check()
-{
-	int i;
-	int count=0;
-
-	for(i=0; i < BOARD_SIZE; i++) //가로
-	{
-		if(board[i][0]==0&&board[i][1]==0&&board[i][2]==0&&board[i][3]==0&&board[i][4]==0) //가로
-		{
-			count++;
-		}
-		if(board[0][i]==0&&board[1][i]==0&&board[2][i]==0&&board[3][i]==0&&board[4][i]==0) //세로
-			count++;
-	}
-	/*
-	for(i=0; i < BOARD_SIZE; i++)
-	{
-		for(j=0; board[i][j]==0||j==BOARD_SIZE; j++)
-		{
-			if(j==BOARD_SIZE)
-			{
-				count++;
-			}
-		}
-	}
-	*/
-	if(board[0][0]==0&&board[1][1]==0&&board[2][2]==0&&board[3][3]==0&&board[4][4]==0)
-		count++;
-	if(board[0][4]==0&&board[1][3]==0&&board[2][2]==0&&board[3][1]==0&&board[4][0]==0)
-		count++;
-	printf("빙고 수: %d\n", count);
-
-	if(count>=5)
-	{
-		flag=0;
-		printf("클라이언트 승리\n");
-	}
 }
--- bingo_server.c
+++ bingo_server.c
@@ -9,19 +9,23 @@
 #define BACKLOG 3 //연결대기 큐 숫자
 
 void error_check(int validation, char *message); //실행 오류 검사
-void game_init(); //빙고판 생성
-void game_run(int number, int turn_count); //게임진행
-void bingo_check();
+void server_game_init(); //빙고판 생성
+void client_game_init(); //빙고판 생성
+void game_print(int turn_count); //빙고판 프린트
+void game_run(int board[][BOARD_SIZE], int number); //빙고판에 숫자 체크
+int bingo_check(int board[][BOARD_SIZE]);
 
-int board[BOARD_SIZE][BOARD_SIZE]; //보드판 배열
-int turn[BOARD_SIZE*BOARD_SIZE]; //턴마다 숫자 기록
+int server_board[BOARD_SIZE][BOARD_SIZE]; //보드판 배열
+int client_board[BOARD_SIZE][BOARD_SIZE]; //보드판 배열
+//int turn[BOARD_SIZE*BOARD_SIZE]; //턴마다 숫자 기록
 int flag=1; //게임 승리여부
 int check_number[BOARD_SIZE*BOARD_SIZE+1]={0};
 int client_fd;
+int turn[5];
 
 void main(int argc, char *argv[])
 {
-	int server_fd, array_len, i, recv_len, recv_count;
+	int server_fd, array_len, i, recv_len, recv_count, j;
 	struct sockaddr_in server_adr, client_adr;
 	socklen_t client_adr_size;
 
@@ -50,40 +54,78 @@ void main(int argc, char *argv[])
 	error_check(client_fd, "연결요청 승인");
 
 	printf("빙고게임을 시작합니다.\n");
-	game_init();
-	
+	server_game_init();
+	client_game_init();
+
 	for(i=0;i<BOARD_SIZE*BOARD_SIZE&&flag;i++)
 	{
-		if(i%2==0) //클라이언트 차례
+		turn[0]=i+1;
+
+		if(i%2==0)
 		{
+			//클라이언트 차례
 			recv_len=0;
-			while(recv_len!=sizeof(int)) // 패킷이 잘려서 올수도 있으므로 예외처리를 위한 조건문
+			while(recv_len!=sizeof(turn)) // 패킷이 잘려서 올수도 있으므로 예외처리를 위한 조건문
 			{
-				recv_count=read(client_fd, &turn[i], sizeof(turn[i]));
+				recv_count=read(client_fd, turn, sizeof(turn));
 				error_check(recv_count, "데이터수신");
 				if(recv_count==0) break;
 				printf("%d 바이트를 수신하였습니다\n", recv_count);
 				recv_len+=recv_count;
 			}
-			check_number[turn[i]]=1;
+			game_run(server_board, turn[3]);
+			game_run(client_board, turn[3]);
+			check_number[turn[3]]=1;
+			turn[2]=bingo_check(client_board);
+			
+			if(turn[2]>=5)
+			{
+				turn[1]=1;
+				array_len=write(client_fd, turn, sizeof(turn));
+				printf("%d 바이트의 게임정보를 전송하였습니다\n", array_len);
+				error_check(array_len, "데이터전송");
+			}
 		}
-		else //서버 차례
+
+		else
 		{
+			//서버 차례
 			while(1)
 			{
-				turn[i]=rand()%25+1;
-				if(check_number[turn[i]]==0)
+				turn[3]=rand()%25+1;
+				if(check_number[turn[3]]==0)
 				{
-					check_number[turn[i]]=1;
+					check_number[turn[3]]=1;
 					break;
 				}
 			}
-			array_len=write(client_fd, &turn[i], sizeof(turn[i]));
-			printf("%d 바이트를 전송하였습니다\n", array_len);
+			game_run(server_board, turn[3]);
+			game_run(client_board, turn[3]);
+			turn[4]=bingo_check(server_board);
+
+			if(turn[4]>=5)
+			{
+				turn[1]=2;
+			}
+			array_len=write(client_fd, turn, sizeof(turn));
+			printf("%d 바이트의 게임정보를 전송하였습니다\n", array_len);
 			error_check(array_len, "데이터전송");
 		}
-		printf("turn[%d]=%d\n",i,turn[i]); //디버깅용
-		game_run(turn[i], i);
+
+		for(j=0;j<5;j++)
+			printf("turn[%d]=%d\n", j, turn[j]);
+		if(turn[1]==1)
+		{
+			printf("클라이언트 승리\n");
+			break;
+		}
+		else if(turn[1]==2)
+		{
+			printf("서버 승리\n");
+			break;
+		}
+		game_print(i);
+		printf("=============\n");
 	}
 
 	close(client_fd);
@@ -104,7 +146,7 @@ void error_check(int validation, char* message)
 		fprintf(stdout, "%s 완료\n", message);
 	}
 }
-void game_init()
+void server_game_init()
 {
 	int check_number[BOARD_SIZE*BOARD_SIZE]={0};
 	int i, j; //카운트용 변수
@@ -114,7 +156,7 @@ void game_init()
 
 	printf("%c[1;33m",27); //터미널 글자색을 노랑색으로 변경
 	
-	printf("@------ 빙고판 현황 ------@\n\n"); 
+	printf("@------ 서버 빙고판 ------@\n\n"); 
 	printf("+----+----+----+----+----+\n"); 
 	for(i=0; i < BOARD_SIZE; i++)
 	{
@@ -127,8 +169,8 @@ void game_init()
 				if(check_number[temp]==0) //중복제거 알고리즘
 				{
 					check_number[temp]=1;
-					board[i][j]=temp+1;
-					printf("| %2d ", board[i][j]); 
+					server_board[i][j]=temp+1;
+					printf("| %2d ", server_board[i][j]); 
 					break;
 				}
 			}
@@ -138,7 +180,36 @@ void game_init()
 	}      
 	printf("%c[0m", 27); //터미널 글자색을 원래색으로 변경
 }
-void game_run(int number, int turn_count)
+void client_game_init()
+{
+	int check_number[BOARD_SIZE*BOARD_SIZE]={0};
+	int i, j; //카운트용 변수
+	int array_len;
+
+	srand(time(NULL)+1); //시간을 인자로 주어서 완전한 랜덤을 구현
+
+	for(i=0; i < BOARD_SIZE; i++)
+	{
+		for(j=0; j < BOARD_SIZE; j++)
+		{
+			while(1)
+			{
+				int temp = rand()%25; //0~24 난수 발생
+				
+				if(check_number[temp]==0) //중복제거 알고리즘
+				{
+					check_number[temp]=1;
+					client_board[i][j]=temp+1;
+					break;
+				}
+			}
+		}
+	}      
+	array_len=write(client_fd, client_board, sizeof(client_board));
+	printf("%d 바이트를 전송하였습니다\n", array_len);
+	error_check(array_len, "데이터전송");
+}
+void game_print(int turn_count)
 {
 	int i, j; //카운트용 변수
 
@@ -152,9 +223,7 @@ void game_run(int number, int turn_count)
 	{
 		for(j=0; j < BOARD_SIZE; j++)
 		{
-			if(board[i][j]==number)
-				board[i][j]=0; //X표 처리
-			if(board[i][j]==0)
+			if(server_board[i][j]==0)
 			{
 				printf("| ");
 				printf("%c[1;31m",27);
@@ -162,15 +231,14 @@ void game_run(int number, int turn_count)
 				printf("%c[1;33m",27);
 			}
 			else
-				printf("| %2d ", board[i][j]); 
+				printf("| %2d ", server_board[i][j]); 
 		}
 		printf("|\n");
 		printf("+----+----+----+----+----+\n"); 
 	}      
 	printf("%c[0m", 27); //터미널 글자색을 원래색으로 변경
-	bingo_check();
 }
-void bingo_check(int turn_count)
+int bingo_check(int board[][BOARD_SIZE])
 {
 	int i;
 	int count=0;
@@ -200,11 +268,18 @@ void bingo_check(int turn_count)
 		count++;
 	if(board[0][4]==0&&board[1][3]==0&&board[2][2]==0&&board[3][1]==0&&board[4][0]==0)
 		count++;
-	printf("빙고 수: %d\n", count);
-
-	if(count>=5)
+	return count;
+}
+void game_run(int board[][BOARD_SIZE], int number)
+{
+	int i, j;
+	
+	for(i=0; i < BOARD_SIZE; i++)
 	{
-		flag=0;
-		printf("서버 승리\n");
+		for(j=0; j < BOARD_SIZE; j++)
+		{
+			if(board[i][j]==number)
+				board[i][j]=0; //X표 처리
+		}
 	}
 }
